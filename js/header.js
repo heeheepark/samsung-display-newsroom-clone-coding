560cee91be9167f80be5b8dbfedb36a921c0ca11
heeheepark <pjh9641@gmail.com>
2023-05-09 17:23:20 +0900

    feat: header 영역 반응형 수정 및 추가

--- js/header.js
+++ js/header.js
@@ -5,32 +5,35 @@ let searchArea = document.querySelector(".search-area");
 let mainMenu = document.querySelector(".main-menu");
 let subMenu = document.querySelectorAll(".submenu");
 let header = document.querySelector(".header");
+let menuBtn = document.querySelector(".menu-btn");
 
 searchBtn.addEventListener("click", function () {
   blackBg.style.display = "block";
   searchBtn.style.display = "none";
   searchCloseBtn.style.display = "block";
-  searchArea.style.top = "0px";
+  searchArea.classList.add("search-area-active");
   searchArea.style.boxShadow = "3px 3px 5px rgba(0, 0, 0, 0.15)";
+  menuBtn.style.display = "none";
 });
 
 searchCloseBtn.addEventListener("click", function () {
   blackBg.style.display = "none";
   searchBtn.style.display = "block";
   searchCloseBtn.style.display = "none";
-  searchArea.style.top = "-208px";
+  searchArea.classList.remove("search-area-active");
   searchArea.style.boxShadow = "none";
+  menuBtn.style.display = "block";
 });
 
 mainMenu.addEventListener("mouseover", function () {
-  header.style.height = "280px";
+  header.classList.add("header-active");
   for (let i = 0; i < subMenu.length; i++) {
     subMenu[i].style.display = "block";
   }
 });
 
 mainMenu.addEventListener("mouseout", function () {
-  header.style.height = "95px";
+  header.classList.remove("header-active");
   for (let i = 0; i < subMenu.length; i++) {
     subMenu[i].style.display = "none";
   }
