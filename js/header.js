c23abc4e1227979a14ac69f207b82cba9198fd99
heeheepark <pjh9641@gmail.com>
2023-05-12 14:32:06 +0900

    feat: 5/11 수정사항 업데이트

--- js/header.js
+++ js/header.js
@@ -9,6 +9,12 @@ let menuBtn = document.querySelector(".menu-btn");
 let gnb = document.querySelector(".gnb");
 let sideMenu = document.querySelector(".side-menu");
 
+// 윈도우 너비
+let winW = window.innerWidth;
+window.addEventListener("resize", function () {
+  winW = window.innerWidth;
+});
+
 searchBtn.addEventListener("click", function () {
   blackBg.classList.add("black-box-active");
   searchBtn.style.display = "none";
@@ -25,17 +31,22 @@ searchCloseBtn.addEventListener("click", function () {
   menuBtn.style.display = "block";
 });
 
-mainMenu.addEventListener("mouseover", function () {
+// gnb hover
+gnb.addEventListener("mouseenter", function () {
   header.classList.add("header-active");
-  for (let i = 0; i < subMenu.length; i++) {
-    subMenu[i].style.display = "block";
+  if (winW > 1024) {
+    for (let i = 0; i < subMenu.length; i++) {
+      subMenu[i].classList.add("submenu-active");
+    }
   }
 });
 
-mainMenu.addEventListener("mouseout", function () {
-  header.classList.remove("header-active");
+gnb.addEventListener("mouseleave", function () {
+  if (winW > 1024) {
+    header.classList.remove("header-active");
+  }
   for (let i = 0; i < subMenu.length; i++) {
-    subMenu[i].style.display = "none";
+    subMenu[i].classList.remove("submenu-active");
   }
 });
 
@@ -43,13 +54,26 @@ menuBtn.addEventListener("click", function () {
   let checkHeaderActive = header.classList.contains("header-active");
   if (checkHeaderActive === false) {
     header.classList.add("header-active");
-    menuBtn.classList.add("manu-btn-active");
+    menuBtn.classList.add("menu-btn-active");
+    document.querySelector("body").style.overflow = "hidden";
+    searchBtn.style.display = "none";
     gnb.classList.add("gnb-active");
-    sideMenu.classList.add("side-menu-active");
   } else {
     header.classList.remove("header-active");
-    menuBtn.classList.remove("manu-btn-active");
+    menuBtn.classList.remove("menu-btn-active");
+    document.querySelector("body").style.overflow = "";
+    searchBtn.style.display = "block";
     gnb.classList.remove("gnb-active");
-    sideMenu.classList.remove("side-menu-active");
   }
 });
+
+// 모바일 메뉴
+// let moreMenuList = document.querySelectorAll(".main-menu > li");
+// for (let i = 0; i < moreMenuList.length; i++) {
+
+//   moreMenuList[i].addEventListener("click", function () {
+//     for(let i = 0; i)
+//     subMenu[i].classList.add("submenu-active");
+//   });
+// }
+// console.log(moreMenuList);
