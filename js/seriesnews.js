ad6eb591dcbafcb131ac49c237953d3683f1b3ee
heeheepark <pjh9641@gmail.com>
2023-05-16 22:04:52 +0900

    feat: 추가 수정 및 주석 제거

--- js/seriesnews.js
+++ js/seriesnews.js
@@ -1,34 +1,6 @@
-// new Swiper(".sn-series-news", {
-//   // 제일 작을때
-//   slidesPerView: 2,
-//   spaceBetween: 12,
-//   simulateTouch: false,
-//   navigation: {
-//     nextEl: ".series-news .sn-next-bt",
-//     prevEl: ".series-news .sn-prev-bt",
-//   },
-
-//   breakpoints: {
-//     750: {
-//       slidesPerView: 4,
-//       spaceBetween: 30,
-//     },
-//     1035: {
-//       slidesPerView: 4,
-//       spaceBetween: 47,
-//     },
-//     1240: {
-//       slidesPerView: 5,
-//       spaceBetween: 20,
-//     },
-//   },
-// });
-
 new Swiper(".sn-series-news", {
-  // 제일 작을때
   slidesPerView: 2,
   spaceBetween: 12,
-  // simulateTouch: false,
   navigation: {
     nextEl: ".series-news .sn-next-bt",
     prevEl: ".series-news .sn-prev-bt",
@@ -37,10 +9,6 @@ new Swiper(".sn-series-news", {
     el: ".swiper-scrollbar",
     draggable: true,
   },
-  // pagination : {
-  //   el : '.sn-series-news',
-  //   clickable : true,
-  // 	},
   breakpoints: {
     750: {
       slidesPerView: 4,
